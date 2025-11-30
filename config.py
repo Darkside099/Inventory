5cdeeb8ec6bcb6105caeac59e4074fe662d8312f
Darkside099 <tarunmawri2005@gmail.com>
2025-12-01 03:38:16 +0530

    DB updated

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', '!@#$%^&*()(*&^%$#@!)')
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         'DATABASE_URL',
-        'mysql+pymysql://root:root@localhost:3306/cloud_inventory_db'
+        'mysql+pymysql://root:root1234@inventory.cmz8mc8mmzsm.us-east-1.rds.amazonaws.com:3306/inventory'
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     ITEMS_PER_PAGE = 20
